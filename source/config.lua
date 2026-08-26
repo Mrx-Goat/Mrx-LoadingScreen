fc41fe231564c3d07f4e53ee621d11ae86ca5014
Mrx-Goat <mrgoat870@gmail.com>
2026-08-25 23:45:18 -0400

    Add source/config.lua

---
+++ source/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.ResourceDisplayName = 'MRX Nexus Loading Screen'
+Config.Version = '1.1.0'
+Config.Author = 'MRX-GOAT'
+Config.Discord = 'discord.gg/YOURINVITE'
+Config.Branding = 'Build Different'
+Config.PrintConsoleBanner = true
+Config.AutoShutdownOnSpawn = true
+Config.FadeOnJoin = true

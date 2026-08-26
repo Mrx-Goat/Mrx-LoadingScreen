b74e81d9b66253b8f80fbf59339a28e0f534d48e
Mrx-Goat <mrgoat870@gmail.com>
2026-08-25 23:46:05 -0400

    Add source/web/assets/settings/team.js

---
+++ source/web/assets/settings/team.js
@@ -0,0 +1,17 @@
+// Put staff photos inside assets/avatars and edit the entries below.
+window.MRX_TEAM = [
+  {
+    name: 'MRX-GOAT',
+    role: 'Founder & Developer',
+    about: 'Owner of the community and creator of custom systems.',
+    avatar: 'assets/avatars/avatar1.jfif',
+    discord: '@mrx_goat'
+  },
+  {
+    name: 'Staff Member',
+    role: 'Administrator',
+    about: 'Community administration and player support.',
+    avatar: 'assets/avatars/avatar2.jfif',
+    discord: '@username'
+  }
+];

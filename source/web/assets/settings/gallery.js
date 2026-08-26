273d0864bb614920876e896fe6a9ba6c37e04703
Mrx-Goat <mrgoat870@gmail.com>
2026-08-25 23:46:11 -0400

    Add source/web/assets/settings/gallery.js

---
+++ source/web/assets/settings/gallery.js
@@ -0,0 +1,18 @@
+// Put gallery images inside assets/gallery.
+window.MRX_GALLERY = [
+  {
+    image: 'assets/gallery/gallery-1.png',
+    title: 'Welcome to the City',
+    caption: 'Replace this image with one of your own server screenshots.'
+  },
+  {
+    image: 'assets/gallery/gallery-2.png',
+    title: 'Community Moments',
+    caption: 'Show businesses, gangs, police, EMS, cars or special scenes.'
+  },
+  {
+    image: 'assets/gallery/gallery-3.png',
+    title: 'Built Different',
+    caption: 'Add or remove entries directly in assets/settings/gallery.js.'
+  }
+];
